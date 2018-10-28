80ec45e9cafe8a2314886778ad099e52499a1c82
Kevin Michael <biobot.mike@gmail.com>
2018-10-29 01:18:15 +0200

    Update documentation

--- theme/simplex/js/main.js
+++ theme/simplex/js/main.js
@@ -1,6 +1,10 @@
+/* eslint-env jquery */
+
+'use strict';
+
 $(document).ready(function () {
-    $("[data-toggle='lightbox']").on("click", function (event) {
-        event.preventDefault();
-        $(this).ekkoLightbox();
-    });
+  $('[data-toggle="lightbox"]').on('click', function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
 });
